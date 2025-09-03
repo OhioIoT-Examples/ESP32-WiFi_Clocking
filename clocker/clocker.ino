df3b327bdb74ad900984ec93dbfa4e85c8100796
Larry Vincek <larry@vincek.com>
2025-09-04 00:32:06 +0900

    cleaned up the layout

--- clocker/clocker.ino
+++ clocker/clocker.ino
@@ -8,18 +8,25 @@
 
 void setup() {
 
-    Serial.begin(115200);
+     Serial.begin(115200);
 
     int cnt = 0;
 
     #define BENCHMARK 0     // adjust whenever you change the code beyond what you are testing
 
+
+
+
     ////////////////////// SETUP //////////////////////////
 
-    bool dummy = false;
+    bool dummy = false;     // we iterate on this variable in the LOOP below, then we print it so
+                            // that the compiler doesn't take it be un-needed code
 
+    // bool is_connect = esp_random() != 0;     // if you are going to test a boolean, don't give it a
+                                                // value the compiler can know ahead of time, like, "true"
 
     ///////////////////////////////////////////////////////
+
 
 
 
@@ -27,16 +34,20 @@ void setup() {
     while (cnt++ < ITERATIONS) {
     ////////////////////// LOOP ///////////////////////////
 
-        dummy = !dummy; // some meaningful action
-        
-   
+        dummy = !dummy; // some meaningful action placed in the loop does that the compiler doesn't eliminate it
 
     ///////////////////////////////////////////////////////
     }
     uint64_t stop = esp_cpu_get_cycle_count();
 
+   
+
+
+
+
+
     Serial.print("\tdummy: ");
-    Serial.print(dummy);        // the compiler executes the above code only if we use that value somewhere
+    Serial.print(dummy);        // we print here, so that the compiler believes it's operations above are meaningful
 
     log(start, stop, ITERATIONS, BENCHMARK);
 
--- src/main.cpp
+++ src/main.cpp
@@ -15,29 +15,39 @@ void setup() {
 
     #define BENCHMARK 0     // adjust whenever you change the code beyond what you are testing
 
+
+
+
     ////////////////////// SETUP //////////////////////////
 
-    bool dummy = false;
+    bool dummy = false;     // we iterate on this variable in the LOOP below, then we print it so
+                            // that the compiler doesn't take it be un-needed code
 
+    // bool is_connect = esp_random() != 0;     // if you are going to test a boolean, don't give it a
+                                                // value the compiler can know ahead of time, like, "true"
 
     ///////////////////////////////////////////////////////
 
 
 
-    uint64_t start = esp_cpu_get_ccount();
+
+    uint64_t start = esp_cpu_get_ccount();  // <- get the start cycle count
     while (cnt++ < ITERATIONS) {
     ////////////////////// LOOP ///////////////////////////
 
-        dummy = !dummy; // some meaningful action
-        
-   
+        dummy = !dummy; // some meaningful action placed in the loop does that the compiler doesn't eliminate it
 
     ///////////////////////////////////////////////////////
     }
-    uint64_t stop = esp_cpu_get_ccount();
+    uint64_t stop = esp_cpu_get_ccount();  // <- get the end cycle count
+
+
+
+
+
 
     Serial.print("\tdummy: ");
-    Serial.print(dummy);        // the compiler executes the above code only if we use that value somewhere
+    Serial.print(dummy);        // we print here, so that the compiler believes it's operations above are meaningful
 
     log(start, stop, ITERATIONS, BENCHMARK);
 
